93ea33ccfe8b55fa957cad06555f2d1458adc055
Paulo <paulocesarhero@gmail.com>
2023-05-10 15:41:44 -0600

    Dockerizacion de la bd mongo

--- dao/mongoConector.py
+++ dao/mongoConector.py
@@ -12,17 +12,17 @@ logging.basicConfig(
 class Conector:
     _host : MongoClient = None
 
-    def __init__(self, uri : str ="mongodb://dev:dev@localhost/?authSource=admin") -> None:
+    def __init__(self, uri : str ="mongodb://dev:dev@localhost:27017/?authSource=admin") -> None:
         try:
             self._host : MongoClient = MongoClient(uri)
             logging.info("Conexión exitosa a la base de datos.")
         except (ConnectionFailure, InvalidURI, ConfigurationError, OperationFailure) as e:
             logging.error(f"Error al conectarse a la base de datos: {e}")
 
-    def conectarUsuarios(self) -> Database:
+    def conectarbd(self) -> Database:
         try:
-            db = self._host.usuarios
-            logging.info("Conexión a la colección 'usuarios' exitosa.")
+            db = self._host.musicooldb
+            logging.info("Conexión a la bd usuarios exitosa.")
             return db
         except (AttributeError, OperationFailure) as e:
             logging.error(f"Error al conectar a la base de datos: {e}")
